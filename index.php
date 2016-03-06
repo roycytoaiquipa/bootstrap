ae0484800d878152aab32e0c3b3507f44cc078b7
roy <roycyto1997@gmail.com>
2016-03-06 13:23:42 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ROY CHIPANA AIQUIPA</title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	 
+
+	 <nav class="navbar navbar-default" role="navigation">
+	 	<div class="container-fluid">
+	 		<!-- Brand and toggle get grouped for better mobile display -->
+	 		<div class="navbar-header">
+	 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+	 				<span class="sr-only">Toggle navigation</span>
+	 				<span class="icon-bar"></span>
+	 				<span class="icon-bar"></span>
+	 				<span class="icon-bar"></span>
+	 			</button>
+	 			<a class="navbar-brand" href="#">Title</a>
+	 		</div>
+	 
+	 		<!-- Collect the nav links, forms, and other content for toggling -->
+	 		<div class="collapse navbar-collapse navbar-ex1-collapse">
+	 			<ul class="nav navbar-nav">
+	 				<li class="active"><a href="#">Link</a></li>
+	 				<li><a href="#">Link</a></li>
+	 			</ul>
+	 			<form class="navbar-form navbar-left" role="search">
+	 				<div class="form-group">
+	 					<input type="text" class="form-control" placeholder="Search">
+	 				</div>
+	 				<button type="submit" class="btn btn-default">Submit</button>
+	 			</form>
+	 			<ul class="nav navbar-nav navbar-right">
+	 				<li><a href="#">Link</a></li>
+	 				<li class="dropdown">
+	 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	 					<ul class="dropdown-menu">
+	 						<li><a href="#">Action</a></li>
+	 						<li><a href="#">Another action</a></li>
+	 						<li><a href="#">Something else here</a></li>
+	 						<li><a href="#">Separated link</a></li>
+	 					</ul>
+	 				</li>
+	 			</ul>
+	 		</div><!-- /.navbar-collapse -->
+	 	</div>
+	 </nav>
+	
+</div>
+
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/1.lnk">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Example headline.</h1>
+					<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="first slide" src="public/img/r.jpg" width="800" height="800">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Another example headline.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/img/roy1.jpg" width="900">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>One more for good measure.</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+<h2 class="roy">EXPORTACION DE AUTOS EN MEXICO</h2>
+<h2 class="mari">
+México se apresta a superar a Brasil como mayor productor latinoamericano de automóviles por primera vez en más de un decenio, en la medida que las exportaciones a los Estados Unidos traen aparejadas la apertura de fábricas y una producción  de récord</h2>
+<img class="t" src="public/img/v.jpg" height="200" width="300">
+<img class="t" src="public/img/t.jpg" height="200" width="300">
+<img class="t" src="public/img/p.jpg" height="200" width="300">
+<img class="t" src="public/img/n.jpg" height="200" width="300">
+<h2 class="M">AUTO DE MARCA VERSA</h2>
+<h2 class="y" >El auto versa es muy  divertido, es eficiente, es espacioso y tiene estilo para arrancar. Sutiles contrastes cromados de parrilla y manijas de puerta cromadas, líneas elegantes y hombros amplios hacen que el Versa se destaque en el estacionamiento.</h2>
+<img class="r" src="public/img/d.jpg" height="500" width="1000">
+<h2  class="minaya">¿QUE ES UN AUTO MOVIL?</h2>
+<h2 class="o">Un automóvil deportivo es un vehículo medio o pequeño, que puede ser para 2 o 4 pasajeros, diseñado para poder circular a altas velocidades. Al contrario que un automóvil de carreras, un automóvil deportivo está pensado para ser conducido en la vía pública. Tienen mejor aceleración, velocidad máxima, adherencia al asfalto, y mayor sistema de frenada que los automóviles normales, lo cual se logra mediante motores, frenos, suspensión, caja de cambios, neumáticos, chasis y carrocería especiales. Las carrocerías asociadas a un deportivo son cupé y descapotable </h2
+<img class="q" src="public/img/nay.jpg" height="200" width="300">
+<img class="w" src="public/img/p.jpg" height="200" width="300">
+<img class="w" src="public/img/roy1.jpg" height="200" width="300">
+<img class="w" src="public/img/1.lnk" height="200" width="300">
+<img class="w" src="public/img/2.lnk" height="200" width="300">
+<h2 class="yony">El MOTOR TELSA</h2>
+<h2 class="poma">Tesla Motors pretende ofrecer al cliente una experiencia convincente mientras que consigue ventas y beneficios de servicio que los fabricantes tradicionales no tienen en el modelo de distribución por franquicias y de talleres oficiales. Los clientes tratan directamente con personal de Tesla y no con franquiciados. Tesla quiere conseguir un mejor control de costes de inventario, gestión de garantía, servicio, fortalecimiento de la marca y obtención de comentarios de los clientes. Al ser propietario de la red comercial Tesla evita el conflicto de intereses en la estructura de los concesionarios tradicionales donde la venta de piezas en garantía y reparaciones del concesionario son un beneficio para el concesionario y un gasto para el fabricante.</h2>
+<h2 class="juan">VIDEOS DE FABRICA DE AUTOS EN MEXICO</h2>
+<div class="container">  
+	<div class="row">
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/89.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/90.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/91.mp4">
+				</video>
+			</div>
+		</div>
+		<h2 class="lulu">POEMA</h2>
+		<h2 class="luna">Es una lástima que no estés conmigo
+			cuando miro el reloj y son las cuatro
+			y acabo la planilla y pienso diez minutos
+			y estiro las piernas como todas las tardes
+			y hago así con los hombros para aflojar la espalda
+			y me doblo los dedos y les saco mentiras.
+			
+
+
+			Es una lástima que no estés conmigo
+			cuando miro el reloj y son las cinco
+			y soy una manija que calcula intereses
+			o dos manos que saltan sobre cuarenta teclas
+			o un oído que escucha como ladra el teléfono
+			o un tipo que hace números y les saca verdades.
+
+
+			
+			Es una lástima que no estés conmigo
+			cuando miro el reloj y son las seis.
+			Podrías acercarte de sorpresa
+			y decirme "¿Qué tal?" y quedaríamos
+			yo con la mancha roja de tus labios
+			tú con el tizne azul de mi carbónico.</h2>
+<div class="container">  
+	<div class="row">
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/Plan B - El Amor No Existe.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/♥POEMA♥♫♥PERDONAME♥.mp4">
+				</video>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="mi-video">
+				<video autobuffer autoloop loop controls height="280" width="350">
+					<source src="public/video/antologia volveré.mp4">
+				</video>
+			</div>
+		</div>
+							<h2 class="paola">::::::R&M:::::</h2>
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+</body>
+</html>
